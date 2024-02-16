c70cb94892d06cf129bc804caa8ec1b11a2efcae
Pranay Namburi <pranayn1999@gmail.com>
2024-02-15 19:33:41 -0500

    Installed jest and added unit test

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ const express = require('express')
 const cors = require('cors')
 const app = express();
 const port = 3000;
+const { add } = require('./arithematica')
 app.use(cors());
 
 app.get('/add/:num1/:num2', (req, res)=>{
-    res.status(200).send({'result' : Number(req.params.num1)+Number(req.params.num2)})
+    res.status(200).send({'result' : add(Number(req.params.num1), Number(req.params.num2))})
 })
 app.get('/',(req, res)=>{
     res.send('Arithematic service');
